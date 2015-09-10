c2c809125a3f7cd4977ca6bc306c1441d68158c1
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-10 15:05:54 -0600

    Finished then fiddled with the timer a bit--same result as without the timer.

---
+++ js/mainCtrl.js
@@ -0,0 +1,5 @@
+app.controller('MainController', [function() {
+	var vm = this;
+	
+	vm.name = "Sam";
+}]);
---
+++ js/timeDirective.js
@@ -0,0 +1,19 @@
+app.directive('showTime', [function() {
+	return {
+		restrict: 'E',
+		template: '<div> The current time is {{ vm.time | date: "hh:mm:ss" }} </div>', 
+		controller: function() {
+			var vm = this;
+			
+			vm.time = new Date();
+			vm.refreshDate = true;
+			for(var i = 0; i < 5; i++) {
+				setTimeout(function() {
+					vm.time = new Date();
+					console.log(vm.time);
+				}, 1000 * i)
+			}
+		},
+		controllerAs: 'vm'
+	}
+}]);
